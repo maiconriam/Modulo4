0746821330c35af830b94faebab8b05542bdc569
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-10-29 11:36:47 -0300

    Criar classe lead

--- src/main/java/br/com/zup/Lead/Lead/DTOS/LeadDTO.java
+++ src/main/java/br/com/zup/Lead/Lead/DTOS/LeadDTO.java
@@ -1,4 +1,48 @@
 package br.com.zup.Lead.Lead.DTOS;
 
+import java.util.List;
+
 public class LeadDTO {
+    private String nome;
+    private String telefone;
+    private String email;
+    private List<ProdutoDTO> listaDeProdutos;
+
+    public LeadDTO() {
+
+    }
+
+    public LeadDTO(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void adicionarProduto(ProdutoDTO novoProduto){
+        listaDeProdutos.add(novoProduto);
+    }
 }
